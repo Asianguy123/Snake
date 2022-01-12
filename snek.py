12de9f2504ea2aa56a9183715c84461fefe6ed21
Asianguy123 <sas@drsyed.org>
2022-01-12 09:51:25 +0000

    Update snek.py

--- snek.py
+++ snek.py
@@ -21,8 +21,12 @@ class Snake():
 
 class Fruit():
     def __init__(self):
-        self.randomise()     
-    
+        self.randomise()
+
+    def draw_fruit(self):
+        fruit_rect = pygame.Rect(int(self.pos.x * CELL_SIZE), int(self.pos.y * CELL_SIZE), CELL_SIZE, CELL_SIZE)
+        SCREEN.blit(apple, fruit_rect)
+
     def randomise(self):
         self.x = random.randint(0, CELL_NUMBER - 1)
         self.y = random.randint(0, CELL_NUMBER - 1) 
